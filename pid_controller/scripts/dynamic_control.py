a5c436d7abf1b513d944e5f510e037a8199ba88a
prakhar2001maheshwari <koolprakhar10@gmail.com>
2020-03-22 21:14:33 +0530

    Minor Changes

--- pid_controller/scripts/dynamic_control.py
+++ pid_controller/scripts/dynamic_control.py
@@ -8,47 +8,64 @@ from underwater_sensor_msgs.msg import DVL
 from PID import PIDRegulator
 
 class controlPID:	
-	def __init__(self):
+	def __init__(self,x,y,z):
 		self.cur_pos=np.zeros(3)
 		self.cur_vel=np.zeros(3)
+		self.ref=np.zeros(3)
+		self.ref[0]=x
+		self.ref[1]=y
+		self.ref[2]=z
 		
 	def update(self):
-		rospy.init_node('Controller', anonymous=True)
 		
 		ref=np.zeros(3)
-		
+		u=np.zeros(5)
 		
 		kp=1
 		ki=0
 		kd=1
 		t=0.01
 		
-		ref[0]=int(input("Enter x co-ordinate"))
-		ref[1]=int(input("Enter y co-ordinate"))
-		ref[2]=int(input("Enter z co-ordinate"))
-		
 		pid=PIDRegulator(kp,ki,kd)
 		
 		sub_pose=rospy.Subscriber('g500/pose',Pose,self.pose_callback)
 		sub_vel=rospy.Subscriber('g500/dvl',DVL,self.vel_callback)
-		pub_acc=rospy.Publisher('g500/thruster_input', Float64MultiArray, queue_size=10)
+		pub_acc=rospy.Publisher('g500/thrusters_input', Float64MultiArray, queue_size=10)
 		 
-		e_pos=ref-self.cur_pos
+		e_pos=self.ref-self.cur_pos
 		cmd_vel=pid.regulate(e_pos,t)
 		e_vel=cmd_vel-self.cur_vel
 		cmd_acc=pid.regulate(e_vel,t)
-		pub_acc.publish(cmd_acc)
+		
+		u[0]=-cmd_acc[1]
+		u[1]=-cmd_acc[1]
+		u[2]=-cmd_acc[2]
+		u[3]=-cmd_acc[2]
+		u[4]=-cmd_acc[0]
+		
+		msg=Float64MultiArray
+		msg.data=u
+		pub_acc.publish(msg)
+		print" Done one iteration"
 
 	#def PID:
 
 
 	def vel_callback(self, msg):
-		self.cur_vel = numpy.array([msg.bi_x_axis, msg.bi_y_axis, msg.bi_z_axis])
+		self.cur_vel = np.array([msg.bi_x_axis, msg.bi_y_axis, msg.bi_z_axis])
 
 	def pose_callback(self,msg):
-		p=msg.Point
-		self.cur_pos=numpy.array([p.x, p.y, p.z])
+		p=msg.position
+		self.cur_pos=np.array([p.x, p.y, p.z])
 
-cnt=controlPID()
+
 print "Controller Node:"
-cnt.update()
+rospy.init_node('Controller', anonymous=True)
+x=int(input("Enter x co-ordinate"))
+y=int(input("Enter y co-ordinate"))
+z=int(input("Enter z co-ordinate"))
+cnt=controlPID(x,y,z)
+while not rospy.is_shutdown():
+	cnt.update()
+	rospy.Rate(100).sleep()
+
